01404d492295e6437eaa3a085531f47334a2b384
Maia Bard <Maiakbard@gmail.com>
2025-02-05 13:25:33 -0700

    Add warm up answers to file

--- lessons/enumerables/warm_up/warm_up.rb
+++ lessons/enumerables/warm_up/warm_up.rb
@@ -1,13 +1,19 @@
 # 1. Using #each, add to the method below, such that it takes in an array of names, 
 # and returns an array of all names upcased.
 
-kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
+# kardashians = ["kris", "kim", "kourtney", "khloe", "rob"]
 
-def upcase_names(names)
- # your code here
-end 
+# def upcase_names(names)
+#  # your code here
+#     upper_kard = []
+#     names.each do |name|
+#     upper_kard << name.upcase
+    
+#     end
+#     upper_kard
+# end 
 
-p upcase_names(kardashians)
+# p upcase_names(kardashians)
 
 # should return ["KRIS", "KIM", "KOURTNEY", "KHLOE", "ROB"]
 
@@ -23,26 +29,33 @@ pets = ["Brutus", "Lucky", "Goldie", "Pepper", "Odie"]
 
 def names_ending_in_ie(names)
  # your code here
+    ie_names = []
+    names.each do |name|
+        if name.end_with?("ie")
+        ie_names << name
+        end
+    end
+    ie_names
 end 
 
 p names_ending_in_ie(pets)
 
-# should return an array of ["Goldie", "Odie"]
+# # should return an array of ["Goldie", "Odie"]
 
 
 
 
 
 
-# 3. Using #each, update the method below, such that it takes in an array of integers,
-# and returns the first number that is over 100 when squared. 
+# # 3. Using #each, update the method below, such that it takes in an array of integers,
+# # and returns the first number that is over 100 when squared. 
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-def first_num_thats_square_is_above_100(nums)
+# def first_num_thats_square_is_above_100(nums)
 
-end 
+# end 
 
-p first_num_thats_square_is_above_100(nums)
+# p first_num_thats_square_is_above_100(nums)
 
-# should return the integer 11
+# # should return the integer 11
